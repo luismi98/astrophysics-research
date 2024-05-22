1d6854648f6beefdb119bee099f8f76003c2ab3b
Luismi <luismi98sanmartin@gmail.com>
2024-05-22 14:00:16 +0200

    tidy up

--- src/errorconfig.py
+++ src/errorconfig.py
@@ -8,7 +8,7 @@ class BootstrapConfig():
             Size of each bootstrap sample. For proper bootstrapping it should be the same as the original sample size (see e.g. https://stats.stackexchange.com/questions/263710).
             The default behaviour, i.e. when `sample_size` is set to None, is to use the original sample size.
         
-        replacement: boolean, optional
+        replace: boolean, optional
             Whether to take the samples with replacement or not. Defaults to True.
         
         repeats: int, optional. Default is 5000
@@ -33,34 +33,34 @@ class BootstrapConfig():
         self.batch_size = batch_size if batch_size is not None else repeats
 
 class MonteCarloConfig():
-    def __init__(self, perturbed_vars, affected_cuts_dict, repeats=500, affected_cuts_lims_dict=None, error_frac=None,symmetric=False,\
+    def __init__(self, perturbed_vars, affected_cuts_dict, repeats=5000, error_frac=None,symmetric=False, affected_cuts_lims_dict=None,\
                   random_resampling_indices=None):
         """
         Parameters
         ----------
         perturbed_vars: list of strings
             List of variables to perturb.
-        error_frac: float or None
-            If float, it is the fractional uncertainty in the measurements.
-            If None, take the error from the actual measurement uncertainties (in the observations).
-        repeats: integer
-            Number of MC repeats.
-        symmetric: boolean
-            If False, the MC method computes the standard deviation for values above and below the true value separately.
-        random_resampling_indices: pandas Index object or None
-            If a pandas Index object, it contains the indices of the stars in the dataframe used to compute the true_value, i.e. after applying
-                                     any of the affected cuts, which were selected after random downsampling. If any of those stars falls outside
-                                     the affected cuts after the perturbation, we incorporate other stars to complete the sample.
-            If None, no downsampling was performed.
         affected_cuts_dict: dictionary or None
             Dictionary of cuts, where each key is a variable (str) and the values are a tuple containing the minimum and maximum values,
             which are affected by the perturbation of any of the perturbed variables. These cuts have to be applied after the MC perturbation
             as the stars might move across the limit (for example when applying a distance perturbation and wanting a cut on R). 
             In the calculation of MC values, we exclude/include the stars which fall outside/inside the limits after the perturbation respectively.
-        affected_cuts_lims_dict: dictionary or None
+        repeats: integer, optional. Default is 5000
+            Number of MC repeats.
+        error_frac: float or None. Default is None
+            If float, it is the fractional uncertainty in the measurements.
+            If None, take the error from the actual measurement uncertainties (which exist in the observations).
+        symmetric: boolean. Default is False.
+            If False, the MC method computes the standard deviation for values above and below the true value separately.
+        affected_cuts_lims_dict: dictionary or None. Default is None.
             Dictionary with the same keys as affected_cuts_dict, where the values are "min", "max", "both" or "neither", indicating the limits
             that are included in the cut. 
             If None, or some variables in affected_cuts_dict are not in the keys, the default is "both".
+        random_resampling_indices: pandas Index object or None. Default is None
+            If a pandas Index object, it contains the indices of the stars in the dataframe used to compute the true value, i.e. after applying
+                                     any of the affected cuts, which were selected after random downsampling. If any of those stars falls outside
+                                     the affected cuts after the perturbation, we incorporate other stars to complete the sample.
+            If None, no downsampling was performed.
         """
         
         self.perturbed_vars = perturbed_vars
@@ -69,9 +69,12 @@ class MonteCarloConfig():
         self.symmetric = symmetric
         self.random_resampling_indices = random_resampling_indices
         
-        self.affected_cuts_dict = {} if affected_cuts_dict is None else affected_cuts_dict
-        self.affected_cuts_lims_dict = {} if affected_cuts_lims_dict is None else affected_cuts_lims_dict
-        
         for k in self.affected_cuts_dict:
             if k not in self.affected_cuts_lims_dict:
                 self.affected_cuts_lims_dict[k] = "both"
+
+        # Set to {} here instead of in the default values of the arguments because default arguments are evaluated only once and hence are mutable.
+        # See, e.g., https://www.30secondsofcode.org/python/s/mutable-default-arguments/
+        self.affected_cuts_dict = {} if affected_cuts_dict is None else affected_cuts_dict
+        self.affected_cuts_lims_dict = {} if affected_cuts_lims_dict is None else affected_cuts_lims_dict
+        
