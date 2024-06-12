8462e2348062e2189536cc01b26d900cd712497f
Luismi <luismi98sanmartin@gmail.com>
2024-06-12 16:24:31 +0200

    helper to get func parameters

--- utils/error_helpers.py
+++ utils/error_helpers.py
@@ -3,6 +3,74 @@ import numpy as np
 import warnings
 import pandas as pd
 
+import src.compute_variables as CV
+
+def get_function_parameters(func_name, vx, vy, bin_range=None):
+    """
+    Return the function and parameters needed to apply the given function in order to appropriately compute the given statistic.
+    
+    Parameters
+    ----------
+    func_name: string.
+        The statistic of interest.
+    vx,vy: 1D numpy arrays
+        The horizontal/vertical velocities.
+    bin_range: list of 2 tuples, optional. Default is None.
+        The limits of the bins in position space. Example: [[-1,1], [3,5]]. Required if working with spherical tilt.
+        
+    Returns
+    -------
+    function: callable.
+        The function to compute the statistic of interest.
+    vx: 1D numpy array, or None.
+        The horizontal velocity values. If the statistic only involves y, such as mean_vy or std_vy, vx will be None.
+    vy: 1D numpy array, or None.
+        The vertical velocity values. If the statistic only involves x, such as mean_vx or std_vx, vy will be None.
+    R_hat: tuple, or None
+        Coordinates of center of bin in position space, used to calculate the spherical tilt.
+    tilt: boolean
+        Whether the function is a vertex deviation.
+    absolute: boolean
+        If `tilt` is True, whether the vertex deviation is computed using the absolute value of the dispersion difference.
+        Otherwise False.
+    """
+    
+    if "spherical" in func_name and bin_range is None:
+        raise ValueError("The bin range is needed to compute the spherical tilt.")
+    
+    if func_name == "mean_vx":
+        return CV.calculate_mean, vx, None, None, False, False
+    
+    elif func_name == "mean_vy":
+        return CV.calculate_mean, None, vy, None, False, False
+    
+    elif func_name == "std_vx":
+        return CV.calculate_std, vx, None, None, False, False
+    
+    elif func_name == "std_vy":
+        return CV.calculate_std, None, vy, None, False, False
+    
+    elif func_name == "anisotropy":
+        return CV.calculate_anisotropy, vx, vy, None, False, False
+    
+    elif func_name == "correlation":
+        return CV.calculate_correlation, vx, vy, None, False, False
+    
+    elif func_name == "tilt_abs":
+        return CV.calculate_tilt, vx, vy, None, True, True
+    
+    elif func_name == "tilt":
+        return CV.calculate_tilt, vx, vy, None, True, False
+    
+    elif func_name == "spherical_tilt":
+        return CV.calculate_spherical_tilt, vx, vy, [np.mean(bin_range[0]), np.mean(bin_range[1])], True, False
+    
+    elif func_name == "abs_spherical_tilt":
+        return CV.calculate_spherical_tilt, vx, vy, [np.mean(bin_range[0]), np.mean(bin_range[1])], True, True
+    
+    else:
+        raise ValueError(f"Behaviour not yet implemented for function name `{func_name}`.")
+
 def apply_function(function, vx, vy, R_hat, tilt, absolute):
     if vx is None and vy is None:
         raise ValueError("At least one of vx and vy must not be None.")
