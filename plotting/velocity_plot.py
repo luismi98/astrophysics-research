596c8c1a6f5aff3c87b3e0b73b66bba128f3bd7e
Luismi <luismi98sanmartin@gmail.com>
2024-06-13 17:01:29 +0200

    fixes

--- plotting/velocity_plot.py
+++ plotting/velocity_plot.py
@@ -1,3 +1,4 @@
+import warnings
 import numpy as np
 import seaborn as sns
 from scipy.ndimage import gaussian_filter
@@ -12,10 +13,13 @@ import plotting.plotting_helpers as PH
 import plotting.map_functions as mapf
 import utils.error_helpers as EH
 
-def velocity_plot(vx, vy, ax=None, bins=30, vel_vx_var=None, vel_vy_var=None, vel_lims=None, bootstrap_repeats=500, ellipse_bool=True,
+def velocity_plot(vx, vy, ax=None, bins=30, vel_x_var=None, vel_y_var=None, vel_lims=None, bootstrap_repeats=500, ellipse_bool=True,
                   contours_bool=True, contour_kws=None, contour_smoothing=1, seaborn_contours=False, contour_cbar_bool=False, 
                   c=None, scatter_colour_cbar=False, scatter_colour_var = None, scatter_cmap = "coolwarm",
-                  text_bool=False, variables_and_plot_positions=None, text_kws=None, axis_labels="both"):
+                  text_bool=False, variables_and_plot_positions=None, text_kws=None, axis_labels="neither"):
+
+    vx = np.array(vx)
+    vy = np.array(vy)
 
     if len(vx) != len(vy):
         raise ValueError("`vx` and `vy` must have the same length!")
@@ -76,21 +80,27 @@ def velocity_plot(vx, vy, ax=None, bins=30, vel_vx_var=None, vel_vy_var=None, ve
         plot_ellipse(ax,vx,vy)
 
     if axis_labels in ["x","both"]:
-        ax.set_xlabel(r"$v_%s$ [km $\rm s^{-1}$]"%vel_vx_var)
+        if vel_x_var is None:
+            warnings.warn("Cannot show the xlabel if `vel_vx_var` is None.")
+        else:
+            ax.set_xlabel(r"$v_%s$ [km $\rm s^{-1}$]"%vel_x_var)
     if axis_labels in ["y","both"]:
-        ax.set_ylabel(r"$v_%s$ [km $\rm s^{-1}$]"%(vel_vy_var if vel_vy_var!="phi" else "\phi"))
+        if vel_y_var is None:
+            warnings.warn("Cannot show the ylabel if `vel_vy_var` is None.")
+        else:
+            ax.set_ylabel(r"$v_%s$ [km $\rm s^{-1}$]"%(vel_y_var if vel_y_var!="phi" else "\phi"))
     
     if text_bool:
         
         if variables_and_plot_positions is None:
             variables_and_plot_positions = {
-                "number": [0.75, 0.97],
-                "anisotropy": [0.025, 0.07],
-                "correlation": [0.64, 0.07],
+                "number": [0.73, 0.97],
+                "anisotropy": [0.025, 0.09],
+                "correlation": [0.53, 0.09],
                 "tilt_abs": [0.025, 0.97],
             }
 
-        show_variable_values_text(ax,vel_x_var=vel_vx_var,vel_vy_var=vel_vy_var,vx=vx,vy=vy,variables_and_plot_positions=variables_and_plot_positions,
+        show_variable_values_text(ax=ax,vel_x_var=vel_x_var,vel_y_var=vel_y_var,vx=vx,vy=vy,variables_and_plot_positions=variables_and_plot_positions,
                                   bootstrap_repeats=bootstrap_repeats, kws=text_kws)
     
     return ax
@@ -103,12 +113,10 @@ def show_variable_values_text(ax, vel_x_var, vel_y_var, vx, vy, variables_and_pl
 
         if text_kws is None:
             text_kws = {}
-        if text_kws["bbox"] is None:
-            textbox_kws = {}
         
         for kw in default_text_kws:
             if kw not in text_kws:
-                textbox_kws[kw] = default_text_kws[kw]
+                text_kws[kw] = default_text_kws[kw]
         for box_kw in default_textbox_kws:
             if box_kw not in text_kws["bbox"]:
                 text_kws["bbox"][box_kw] = default_textbox_kws[box_kw]
@@ -123,7 +131,7 @@ def show_variable_values_text(ax, vel_x_var, vel_y_var, vx, vy, variables_and_pl
 
     for variable in variables_and_plot_positions:
         if "spherical" in variable:
-            raise ValueError("Did not expect a spherical tilt")
+            raise ValueError("Did not expect a spherical tilt. It needs information about the bin edges.")
         
         x,y = variables_and_plot_positions[variable]
 
@@ -133,12 +141,12 @@ def show_variable_values_text(ax, vel_x_var, vel_y_var, vx, vy, variables_and_pl
 
         symbol,units = kinematic_symbols_dict[variable], kinematic_units_dict[variable]
 
-        func, vx, vy, _, tilt, absolute = EH.get_function_parameters(variable, vx=vx, vy=vy)
+        func, vx, vy, R_hat, tilt, absolute = EH.get_function_parameters(variable, vx=vx, vy=vy)
 
-        value = EH.apply_function(function=func, vx=vx,vy=vy,tilt=tilt,absolute=absolute)
+        value = EH.apply_function(function=func, vx=vx,vy=vy,tilt=tilt,absolute=absolute,R_hat=R_hat)
         error = bootstrap.get_std_bootstrap(function=func,config=bootstrapconfig, vx=vx,vy=vy,tilt=tilt,absolute=absolute).confidence_interval[0]
 
-        text = fr"${symbol}={value:.2f} \pm {error:.2f} {units}$"
+        text = fr"{symbol}$={value:.2f} \pm {error:.2f} {units}$"
 
         ax.text(x,y,text,transform=ax.transAxes,**kws)
 
@@ -150,7 +158,7 @@ def plot_ellipse(ax,vx,vy,radius_factor=2,max_vector_factor=2.5):
     ratio = np.sqrt(min(eigenvalues)/max(eigenvalues))
     centre = [np.mean(vx), np.mean(vy)]
     x_ellipse, y_ellipse = PH.get_ellipse_coords(radius, ratio, CV.calculate_tilt(vx,vy,absolute=False))
-    ax.plot(x_ellipse+centre[0], y_ellipse+centre[1], lw=4,color='red')
+    ax.plot(x_ellipse+centre[0], y_ellipse+centre[1], lw=3,color='red')
     eigvectors = np.linalg.eig(cov)[1]
     raw_max_vector = eigvectors[:,eigenvalues.argmax()]
     max_vector = raw_max_vector*max_vector_factor*np.sqrt(max(eigenvalues))
@@ -158,4 +166,4 @@ def plot_ellipse(ax,vx,vy,radius_factor=2,max_vector_factor=2.5):
     vector_plot_data = [[max_vector[0], -max_vector[0]],[max_vector[1], -max_vector[1]]]
     vector_plot_data[0] += centre[0]
     vector_plot_data[1] += centre[1]
-    ax.plot(vector_plot_data[0],vector_plot_data[1], lw=4, color='red')
+    ax.plot(vector_plot_data[0],vector_plot_data[1], lw=3, color='red')
